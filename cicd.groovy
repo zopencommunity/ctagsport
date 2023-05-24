d69e0a772bfbc1375c67e2257988736354f88c15
Igor Todorovski <itodorov@ca.ibm.com>
2023-05-24 13:29:05 -0400

    add polling to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,14 @@
 node('linux')
 {
+   
+  stage (Poll) {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/ctagsport.git"]]])
+        }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/ctagsport.git'), string(name: 'PORT_DESCRIPTION', value: 'ctags generates an index (or tag) file of language objects found in source files for programming languages.' )]
   }
