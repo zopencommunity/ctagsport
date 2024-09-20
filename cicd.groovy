9c977fa52826e042e65f14eaa40b41b46c3067e5
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:11 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/ctagsport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/ctagsport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/ctagsport.git'), string(name: 'PORT_DESCRIPTION', value: 'ctags generates an index (or tag) file of language objects found in source files for programming languages.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/ctagsport.git'), string(name: 'PORT_DESCRIPTION', value: 'ctags generates an index (or tag) file of language objects found in source files for programming languages.' )]
   }
 }
